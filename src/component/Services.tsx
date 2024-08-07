3d9de4e49ac70a5a69268b58cbbff0eccb2e6b1f
Afiya32 <biplobguru123@gmail.com>
2024-08-07 13:43:43 +0600

    60% work done

--- src/component/Services.tsx
+++ src/component/Services.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import ServiceCard from './serviceCard';
 const Services = () => {
   return (
-    <div>
-      <h1 > This is  Services  page </h1>
+    <div className='text-center'>
+     <div className='w-2/6 mt-5 mx-auto text-left'>
+     <h1 className='font-normal text-xl' >SERVICES </h1>
+      <h1 className='mt-2 font-semibold text-5xl'>OUR MARKING</h1>
+      <h1 className='mt-2 font-semibold text-5xl'>SERVICES</h1>
+     </div>
+     <div>
+        <ServiceCard/>
+     </div>
     </div>
   );
 };
---
+++ src/component/serviceCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+const ServiceCard = () => {
+  return (
+    <div className='flex justify-between mt-5 items-center'>
+      <h1 > image</h1>
+      <h1 > price details name rate </h1>
+      <h1 > button</h1>
+    </div>
+  );
+};
+
+export default ServiceCard;
